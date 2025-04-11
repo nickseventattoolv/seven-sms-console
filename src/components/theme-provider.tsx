4e241ea660ae6fef748dcd0a29681a108f3ed282
nickseventattoolv <nick@seventattoolv.com>
2025-04-11 16:08:35 -0700

    Fix ThemeProvider by hardcoding required props

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -3,15 +3,11 @@
 import * as React from 'react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 
-export function ThemeProvider({
-  children,
-  ...props
-}: {
-  children: React.ReactNode
-  attribute: string
-  defaultTheme?: string
-  enableSystem?: boolean
-}) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <NextThemesProvider attribute="class" defaultTheme="dark" enableSystem>
+      {children}
+    </NextThemesProvider>
+  )
 }
 
