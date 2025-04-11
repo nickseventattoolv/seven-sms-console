e97eea1d79b15506ed2bf0e94f75f6bca7c32297
nickseventattoolv <nick@seventattoolv.com>
2025-04-11 16:12:34 -0700

    Fix: Correct ThemeProvider typing to support props

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,13 +1,9 @@
 'use client'
 
 import * as React from 'react'
-import { ThemeProvider as NextThemesProvider } from 'next-themes'
+import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from 'next-themes'
 
-export function ThemeProvider({ children }: { children: React.ReactNode }) {
-  return (
-    <NextThemesProvider attribute="class" defaultTheme="dark" enableSystem>
-      {children}
-    </NextThemesProvider>
-  )
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
 
